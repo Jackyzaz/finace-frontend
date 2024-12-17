739748ffab99f2074a97fc1bb95db98e0da19259
SoravitSukkarn <soravit.sukkarn@gmail.com>
2024-12-17 22:55:49 +0700

    add: dashboard

--- src/App.js
+++ src/App.js
@@ -5,23 +5,37 @@ import LoginPage from "./pages/Login";
 import ProfilePage from "./pages/Profile";
 import { AuthProvider } from "./context/useAuth";
 import { ProtectedRoute } from "./context/ProtectedRoute";
+import { Layout } from "antd";
+import HeaderNavbar from "./components/HeaderNavbar";
+import DashboardPage from "./pages/Dashboard";
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <ProfilePage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </AuthProvider>
+    <Layout>
+      <HeaderNavbar />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </Layout>
   );
 }
 
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+export default function DashboardPage() {
+  return (
+    <>
+    <div className="container">
+      <h1>This is Dashboard1234</h1>
+      <h1>This is Dashboard1234</h1>
+    </div>
+    </>
+  );
+}
