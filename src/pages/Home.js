889914c598adececa660011ffd0cfab01d621f9d
SoravitSukkarn <soravit.sukkarn@gmail.com>
2024-12-20 19:09:19 +0700

    add: Home screen title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,21 @@
+import { Col, Row, Space, Typography } from "antd";
 import { useAuth } from "../context/useAuth";
 
 export default function HomePage() {
   const { user } = useAuth();
   return (
     <>
-      <div className="container">
-        <h1>This is home</h1>
-        {user ? <a href="/dashboard">Go to Dashboard</a> : <a href="/login">Go to Login</a>}
-      </div>
+      <Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
+        <Col>
+          <Space direction="vertical" style={{ width: "100%", textAlign: "center" }}>
+            <h1 style={{ fontSize: "8vh" }}>Welcome to My Finance App</h1>
+            <p style={{ fontSize: "4vh" }}>Manage your finances effectively with our modern tools and insights.</p>
+            <p style={{ marginTop: "4vh", fontSize: "4vh" }}>
+              {user ? <a href="/dashboard">Go to Dashboard</a> : <a href="/login">Go to Login</a>}
+            </p>
+          </Space>
+        </Col>
+      </Row>
     </>
   );
 }
